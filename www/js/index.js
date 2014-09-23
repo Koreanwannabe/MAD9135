74b07bfe93a3153cb18effdda4b44300aa275cce
Rebecca <rebeccamargreatwalker@gmail.com>
2014-09-23 18:21:54 -0400

    geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.prepareBtn();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +46,57 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    prepareBtn: function(){
+        var button = document.getElementById ("Location");
+        button.addEventListener("click", this.getGeolocation);
+        console.log ("prepareBtn");
+    },
+
+    getGeolocation: function(){
+        console.log ("Hello");
+     //geolocation position lat and long  
+        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
+    },
+    onSuccess: function(position){
+       alert('Lat: ' + position.coords.latitude + ' ' +  'Lon: ' + position.coords.longitude);  
+       var xhr = new XMLHttpRequest();
+       xhr.open("GET","http://open.mapquestapi.com/geocoding/v1/reverse?key=Fmjtd|luur2hurn0%2Cbg%3Do5-9wasly&location=" + position.coords.latitude + "," + position.coords.longitude,true);
+    
+      xhr.addEventListener('load',function(){
+      if(xhr.status === 200){
+          //alert("We got data: " + xhr.response);
+          console.log(xhr.response);
+          var text= JSON.parse(xhr.response);
+          console.log (text.results[0].locations[0].adminArea5);
+          console.log (text.results[0].locations[0].latLng.lat+" "+text.results[0].locations[0].latLng.lng);
+          document.getElementById("city").innerHTML=text.results[0].locations[0].adminArea5;
+          document.getElementById("latLng").innerHTML+="  Lat    "+text.results[0].locations[0].latLng.lat;
+            document.getElementById("latLng").innerHTML+="  Lng "+text.results[0].locations[0].latLng.lng;
+      }
+    },false); 
+
+    // perform the work
+    xhr.send();
+
+    },
+    // onError Callback receives a PositionError object
+    onError: function(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
     }
+    
+    //Create the XHR object to do GET to /data resource  
+//     XmlRequest: function(){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET","http://open.mapquestapi.com/geocoding/v1/reverse?" + "key=Fmjtd|luur2hurn0%2Cbg%3Do5-9wasly&location=" + position.coords.latitude + "," + position.coords.longitude,true);
+
+//     // register the event handler
+  
+// }
+    //geolocation position lat and long 
+    // navigator.geolocation.getCurrentPosition(function(position){  
+    //          alert('Lat: ' + position.coords.latitude + ' ' +  'Lon: ' + position.coords.longitude); 
+    // });  
 };
